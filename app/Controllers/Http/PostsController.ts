3256b4cd98f0468a5918e55a50bf4c11c6405982
Matheus Carvalho <mathheus.santos.hcs@gmail.com>
2021-10-11 19:14:43 -0300

    Validations

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -1,5 +1,9 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Post from 'App/Models/Post';
+import {
+  StoreValidator,
+  UpdateValidator
+} from 'App/Validators/Post';
 
 export default class PostsController {
   public async index ({}: HttpContextContract) {
@@ -9,7 +13,7 @@ export default class PostsController {
   }
 
   public async store ({ request }: HttpContextContract) {
-    const data = request.only(['title', 'content']);
+    const data = await request.validate(StoreValidator);
     const post = await Post.create(data);
 
     return post;
@@ -23,7 +27,7 @@ export default class PostsController {
 
   public async update ({ request, params }: HttpContextContract) {
     const post = await Post.findOrFail(params.id);
-    const data = request.only(['title', 'content']);
+    const data = await request.validate(UpdateValidator);
 
     post.merge(data);
 
